fa01c1a1b5dbd50e1dc732540e37fad7858f076c
rikisommers <rikisommers@yahoo.co.nz>
2026-03-01 09:34:19 +1300

    fix(02-01): use next/dynamic ssr:false for AtuiDevTest to avoid hydration issues with Stencil web components

--- src/app/dev-test/page.tsx
+++ src/app/dev-test/page.tsx
@@ -1,4 +1,6 @@
-import AtuiDevTest from '@/components/AtuiDevTest';
+import dynamic from 'next/dynamic';
+
+const AtuiDevTest = dynamic(() => import('@/components/AtuiDevTest'), { ssr: false });
 
 export default function DevTestPage() {
   return <AtuiDevTest />;
